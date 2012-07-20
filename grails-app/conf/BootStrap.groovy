627409759b455950ddf67f75d471ab5f64a5b083
yamiao <yaojiamiao2011@gmail.com>
2012-07-20 19:14:53 +0800

    集成了完整的 Shiro.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,13 +5,18 @@ import org.apache.shiro.crypto.hash.Sha512Hash
  //import org.icress.demo.shiro.User
  
 class BootStrap {
-
+/**
 def init = { servletContext ->  
 def user = new ShiroUser(username: "user", passwordHash: new Sha512Hash("user").toHex() ) 	 
 user.save() 	 
 def role= new ShiroRole(name:"admin").addToUsers(user).save() 		     
 } 
-
+**/
+    def init = { servletContext ->
+         def admin = new ShiroUser( username: "admin" ,  passwordHash: new Sha512Hash("admin").toHex()).save()
+         def adminRole = new ShiroRole(name:"admin").addToUsers(admin).addToPermissions("*:*").save()
+     }
+ 
     def destroy = {
     }
 }
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,10 +1,9 @@
 // Place your Spring DSL code here
-import org.apache.shiro.authc.credential.Sha512CredentialsMatcher;
-
+import org.apache.shiro.authc.credential.Sha512CredentialsMatcher
+ 
 beans = {
-credentialMatcher(Sha512CredentialsMatcher) {
-		storedCredentialsHexEncoded = true
-	}
-
-
-}
+     credentialMatcher(Sha512CredentialsMatcher) {
+         storedCredentialsHexEncoded = true
+     }
+ }
+ 
